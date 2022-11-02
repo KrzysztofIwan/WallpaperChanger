6a0007c9f75c05c784a55c7d5fabff673cb87dd6
Krzysztof Iwański <Krzysztof.Iwanski@microsoft.wsei.edu.pl>
2022-11-02 11:57:01 +0100

    Add project files.

---
+++ WallpaperChanger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ChangeBackgount
+{
+    class BackGroundChanger
+    {
+        // used to set wallpaper
+        public const int SPI_SETDESKWALLPAPER = 20;
+        public const int SPIF_UPDATEINFILE = 1;
+        public const int SPIF_SENDCHANGE = 2;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        static void Main(String[] args)
+        {
+            Random random = new Random();
+            int index = random.Next(0, 5);
+            String[] imageNames = { "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" };
+            String imagePath = @"E:\backgrounds\" + imageNames[index];
+
+            //Set wallpaper
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, imagePath, SPIF_UPDATEINFILE | SPIF_SENDCHANGE);
+
+        }
+    }
+}
